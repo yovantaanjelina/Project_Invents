0dbc730c0761a54ff336ffededc2b13ebaefb44e
dwililiya07 <69136411+dwililiya07@users.noreply.github.com>
2021-01-18 19:03:36 +0700

    postingan

---
+++ postingan.php
@@ -0,0 +1,90 @@
+<?php include('navbar.php')?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Postingan</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+<br>
+<div class="row">
+  <div class="col-sm-6"></div>
+  <!-- <div class="col-sm-4"></div> -->
+  <div class="col-sm-6">
+    <form class="form-inline">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text"><img src="loupe.png" width="25px" height=25px"> </span>
+        </div>
+          <input type="text" style="width: 300px;" class="form-control" placeholder="Temukan event yang kamu cari!!">
+      </div>    
+    </form>
+  </div>
+</div>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card" style="width:500px;">
+                    <img class="card-img-top" src="seminar2.jpg" alt="Card image" style="width:100%"><br>
+                    <div class="row">
+                    <div class="col-sm-6"><a href="#"><img class="card-img-top" src="view.png" alt="Card image" style="width:22%">1000 kali dilihat</a></div>
+                    <div class="col-sm-2"><a href="#"><img class="card-img-top" src="chatting.png" alt="Card image" style="width:80%"></a></div>
+                    <div class="col-sm-2"><a href="#"><img class="card-img-top" src="download.png" alt="Card image" style="width:80%"></a></div>
+                    <div class="col-sm-2"><a href="#"><img class="card-img-top" src="share1.png" alt="Card image" style="width:80%"></a></div>
+                    </div>
+                </div>
+                <br>
+                <div class="card"style="width:500px;">
+                <input type="text" class="form-control" placeholder="komentar">
+                    <div class="card-body">
+                        <a href="#"><img class="card-img-top" src="user.png" alt="Card image" style="width:10%"> username</a> mengomentari postingan ini<br><br>
+                        <a href="#"><img class="card-img-top" src="user.png" alt="Card image" style="width:10%"> username</a> mengomentari postingan ini<br><br>
+                        <a href="#"><img class="card-img-top" src="user.png" alt="Card image" style="width:10%"> username</a> mengomentari postingan ini<br><br>
+                        <a href="#"><img class="card-img-top" src="user.png" alt="Card image" style="width:10%"> username</a> mengomentari postingan ini<br><br>
+                    </div> 
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card"style="width:600px;">
+                    <div class="card-body">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore 
+                    magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                    non proident, sunt in culpa qui officia deserunt mollit 
+                    anim id est laborum.<br><br>
+
+                    Tanggal : 22 - 01 - 2021 <br>
+                    contact person : 0893 1212 3027 <br><br>
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore 
+                    magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore 
+                    magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea <br>
+                    commodo consequat. Duis aute irure dolor in 
+                    </div> 
+                    <div class="card-footer">Link pendaftaran: https://bit.ly/IESWeb04</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<?php include('footer.php')?>
+</html>
